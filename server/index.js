52cb3cfb2f46c606ab6ec69158cf2d83339c254b
BhushanJire <bhushanjire@rapidera.com>
2021-03-14 17:44:02 +0530

    first-commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const allRoutes = require('./src/routes/index')
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(allRoutes);
+app.listen(5000, (req, res) => {
+    console.log('Sever is running on port 5000');
+})
